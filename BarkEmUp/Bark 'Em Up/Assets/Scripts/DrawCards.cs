79c89b566fd8a37bc402876519ebfdd6371e2b09
huaac <74689665+huaac@users.noreply.github.com>
2024-01-13 01:09:46 -0800

    correct import of Mirror

---
+++ BarkEmUp/Bark 'Em Up/Assets/Scripts/DrawCards.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DrawCards : NetworkBehaviour
+{
+    public PlayerManager playerManager;
+
+    public void OnClick()
+    {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+        playerManager.CmdDealCards();
+
+    }
+
+}
